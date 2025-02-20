92a2df26db33a7a23f5a113f217f48722aefdf67
zs <zsdfbb@126.com>
2025-02-20 14:46:40 +0800

    add 100
    Signed-off-by: Jack <zsdfbb@126.com>

--- 100_isSameTree/main.c
+++ 100_isSameTree/main.c
@@ -7,12 +7,35 @@
  * };
  */
 
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
+#include "../common.h"
 
-bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-    
+#include "stdlib.h"
+#include "stdbool.h"
+
+struct TreeNode {
+    int val; 
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q)
+{
+    if (p == NULL && q == NULL) {
+        return  true;
+    }
+
+    if (p == NULL && q != NULL) {
+        return false;
+    }
+
+    if (p != NULL && q == NULL) {
+        return false;
+    }
+
+    if (p->val != q->val) {
+        return false;
+    }
+
+    return isSameTree(p->left, q->left)
+            && isSameTree(p->right, q->right);
 }
